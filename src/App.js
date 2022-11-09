6ba8d6b96e51f032ea5336886730748611d9e3f0
giovanaveloso <giovanavelosog@gmail.com>
2022-11-08 22:34:25 -0300

    ongonig

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCards from './components/IdCards';
 
 function App() {
+  const className = "Maria"
+  const classLastName = "Silva"
+  const classGender = "feminino" || "masculino"
+  const classHeight = 70
+  const classBirth ={("04-15-1988")}
+
+  const picture="https://github.com/giovelos/lab-react-training/blob/master/public/maxence-glasses.png"
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>IdCards</h1>
+        <p>Nome{className}</p>
+        <p>Sobrenome{classLastName}</p>
+        <p>Gênero{classHeight}</p>
+        <p>Aniversário{classBirth}</p>     
+        <img src={picture} alt="maxence" />            
+        
+        <IdCards/>
       </header>
     </div>
   );
---
+++ src/components/IdCards.js
@@ -0,0 +1,14 @@
+ function IdCards() {
+
+    return (
+        <div>
+        <h1>Nome</h1>
+        <h2>Sobrenome</h2>
+        <h3 ></h3>
+
+        </div>
+
+    )
+
+ }
+ export default IdCards;
